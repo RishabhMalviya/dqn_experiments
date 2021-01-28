9c487b9313416eb4ef55bb3d9627dc662dd77d76
Rishabh Malviya <rishabh.malviya@gmail.com>
2021-01-28 23:03:35 +0530

    solve cartpole with dueling DQN

--- cart-pole/model.py
+++ cart-pole/model.py
@@ -25,11 +25,13 @@ class DuelingDQN(nn.Module):
         self.seed = torch.manual_seed(seed)
 
         self.fc1 = nn.Linear(state_size, state_size*6)
-        self.fc2 = nn.Linear(state_size*6, state_size*4)
+        self.fc2 = nn.Linear(state_size*6, state_size*6)
 
-        self.fc_state_1 = nn.Linear(state_size*4, 1)
-
-        self.fc_advantages_1 = nn.Linear(state_size*4, action_size)
+        self.fc_state_1 = nn.Linear(state_size*6, state_size*4)
+        self.fc_state_2 = nn.Linear(state_size*4, 1)
+        
+        self.fc_advantages_1 = nn.Linear(state_size*6, state_size*4)
+        self.fc_advantages_2 = nn.Linear(state_size*4, action_size)
         
     def forward(self, state):
         # Common Part
@@ -37,10 +39,12 @@ class DuelingDQN(nn.Module):
         hidden2 = F.relu(self.fc2(hidden1))
         
         # State Value Branch
-        state_value = F.relu(self.fc_state_1(hidden2))
+        state_value_hidden = F.relu(self.fc_state_1(hidden2))
+        state_value = self.fc_state_2(state_value_hidden)
 
         # Advantage Values Branch
-        advantage_values = F.relu(self.fc_advantages_1(hidden2))
+        advantage_values_hidden = F.relu(self.fc_advantages_1(hidden2))
+        advantage_values = self.fc_advantages_2(advantage_values_hidden)
         
         # Recombine branches with equation (9) from the paper (https://arxiv.org/pdf/1511.06581.pdf)
         action_values = state_value + (advantage_values - advantage_values.mean(dim=1).view(-1,1))
